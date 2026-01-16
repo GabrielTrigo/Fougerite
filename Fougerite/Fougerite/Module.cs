39ad5ad1d97c37a8d65026c4381dfa10ef778a7b
DreTaX <dretax14@gmail.com>
2026-01-16 15:56:39 +0100

    Move C# timers to module class instead.

--- Fougerite/Fougerite/Module.cs
+++ Fougerite/Fougerite/Module.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fougerite.Concurrent;
+using Fougerite.Events;
 
 namespace Fougerite
 {
@@ -7,6 +11,9 @@ namespace Fougerite
     /// </summary>
     public abstract class Module : IDisposable
     {
+        public readonly ConcurrentDictionary<string, TimedEvent> Timers = new ConcurrentDictionary<string, TimedEvent>();
+        public readonly ConcurrentList<TimedEvent> ParallelTimers = new ConcurrentList<TimedEvent>();
+        
         public virtual string ModuleFolder { get; set; }
 
         public virtual string Name
@@ -62,5 +69,127 @@ namespace Fougerite
         public abstract void DeInitialize();
 
         public abstract void Initialize();
+        
+        
+        /// <summary>
+        /// Creates a timer with a callback.
+        /// </summary>
+        /// <param name="name">Name of the timer.</param>
+        /// <param name="timeoutDelay">Interval in milliseconds.</param>
+        /// <param name="callback">The callback function to execute when timer fires.</param>
+        /// <param name="autoReset">True if the timer should repeat, false for single execution.</param>
+        /// <returns>The created TimedEvent instance.</returns>
+        public TimedEvent CreateTimer(string name, int timeoutDelay, Action<TimedEvent> callback, bool autoReset = false)
+        {
+            TimedEvent timedEvent = GetTimer(name);
+            if (timedEvent != null)
+            {
+                return timedEvent;
+            }
+
+            UnityEngine.GameObject go = new UnityEngine.GameObject($"{Name}_{name}_{UnityEngine.Random.Range(1, 999999)}");
+            UnityEngine.Object.DontDestroyOnLoad(go);
+            timedEvent = go.AddComponent<TimedEvent>();
+            timedEvent.Name = name;
+            timedEvent.PluginName = Name;
+            timedEvent.Interval = timeoutDelay;
+            timedEvent.AutoReset = autoReset;
+            timedEvent.OnFire += new TimedEvent.TimedEventFireDelegate(callback);
+            timedEvent.OnKilled += (cbName) => Timers.TryRemove(cbName);
+            Timers.Add(name, timedEvent);
+
+            return timedEvent;
+        }
+
+        /// <summary>
+        /// Creates a parallel timer with arguments and a callback. Multiple timers with the same name can exist.
+        /// </summary>
+        /// <param name="name">Name of the timer.</param>
+        /// <param name="timeoutDelay">Interval in milliseconds.</param>
+        /// <param name="args">Dictionary of custom arguments to pass to the timer.</param>
+        /// <param name="callback">The callback function to execute when timer fires.</param>
+        /// <param name="autoReset">True if the timer should repeat, false for single execution.</param>
+        /// <returns>The created TimedEvent instance.</returns>
+        public TimedEvent CreateParallelTimer(string name, int timeoutDelay, Dictionary<string, object> args, Action<TimedEvent> callback, bool autoReset = false)
+        {
+            UnityEngine.GameObject go = new UnityEngine.GameObject($"{Name}_Parallel_{name}_{UnityEngine.Random.Range(1, 999999)}");
+            UnityEngine.Object.DontDestroyOnLoad(go);
+            TimedEvent timedEvent = go.AddComponent<TimedEvent>();
+            timedEvent.Name = name;
+            timedEvent.PluginName = Name;
+            timedEvent.Interval = timeoutDelay;
+            timedEvent.Args = args;
+            timedEvent.AutoReset = autoReset;
+            timedEvent.OnFire += new TimedEvent.TimedEventFireDelegate(callback);
+            timedEvent.OnKilled += (cbName) => ParallelTimers.Remove(timedEvent);
+            ParallelTimers.Add(timedEvent);
+
+            return timedEvent;
+        }
+
+        /// <summary>
+        /// Gets the timer.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public TimedEvent GetTimer(string name)
+        {
+            TimedEvent result = Timers.ContainsKey(name) ? Timers[name] : null;
+            return result;
+        }
+
+        /// <summary>
+        /// Kills the timer.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        public void KillTimer(string name)
+        {
+            TimedEvent timer = GetTimer(name);
+            if (timer == null)
+                return;
+            timer.Kill();
+        }
+        
+        /// <summary>
+        /// Gets the parallel timer.
+        /// </summary>
+        /// <returns>The parallel timer.</returns>
+        /// <param name="name">Name.</param>
+        public List<TimedEvent> GetParallelTimer(string name)
+        {
+            return ParallelTimers.Where(timer => timer.Name == name).ToList();
+        }
+
+        /// <summary>
+        /// Kills the parallel timer.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        public void KillParallelTimer(string name)
+        {
+            foreach (TimedEvent timer in GetParallelTimer(name))
+            {
+                timer.Kill();
+                ParallelTimers.Remove(timer);
+            }
+        }
+
+        /// <summary>
+        /// Kills the timers.
+        /// </summary>
+        public void KillTimers()
+        {
+            foreach (TimedEvent current in Timers.Values)
+            {
+                current.Kill();
+            }
+
+            foreach (TimedEvent timer in ParallelTimers)
+            {
+                timer.Kill();
+            }
+
+            Timers.Clear();
+            ParallelTimers.Clear();
+        }
     }
 }
